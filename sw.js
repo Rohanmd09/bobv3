1283db87231eca243deaf031a0e2063f7384db9a
Rohanmd09 <09rohanmd@gmail.com>
2026-07-31 07:33:08 +0530

    Update CACHE_NAME to vertex-bank-v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vertex-bank-v2';
+const CACHE_NAME = 'vertex-bank-v3';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
